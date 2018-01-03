2b1f2647e1bdb441bf9cc78ba217306e2c49442a
Oliver Steele <steele@osteele.com>
2018-01-03 11:13:14 -0500

    Clear lru_cache by default; add --no-clear option

--- callgraph/extension.py
+++ callgraph/extension.py
@@ -24,16 +24,20 @@ class CallGraphMagics(Magics):
         On completion, the functions are restored to their original values.
 
         The magic knows about functions that are decorated with functools.lru_cache.
-        Two calls with the same arguments to a cached function will display as the same
-        node. For simplicity, "same" to the instrumentation just means same string representation.
-        It also ignores the `maxsize` and `typed` arguments to lru_cache.
+        Two calls with the same arguments to a cached function will display as
+        the same node. For simplicity, "same" to the instrumentation just means
+        same string representation. It also ignores the `maxsize` and `typed`
+        arguments to lru_cache.
 
         Options:
-        -r: reverse the graph (display arrows from callee to caller). Label the arrows with the return values.
 
         -h: display the graph “horizontally”, with function calls running from left to right.
 
+        -r: reverse the graph (display arrows from callee to caller). Label the arrows with the return values.
+
         -w<N>: max width of the graph
+
+        --no-clear: Don't clear lru_cache caches before execution.
 
         Examples
         --------
@@ -57,7 +61,7 @@ class CallGraphMagics(Magics):
         filename = '<magic callgraph>'
         mode = 'exec'
 
-        opts, stmt = self.parse_options(line, 'w:ehr', posix=False)
+        opts, stmt = self.parse_options(line, 'w:ehr', 'no-clear', posix=False)
 
         # Parse the options. Create a call graph recorder with those options.
         options = {'graph_attrs': {}}
@@ -74,14 +78,20 @@ class CallGraphMagics(Magics):
         # Parse the statement. Collect calls to instrument.
         tree = ast.parse(stmt, filename=filename, mode=mode)
         calls = [n for n in ast.walk(tree) if isinstance(n, ast.Call)]
-        fns = [n.func.id for n in calls if isinstance(n.func, ast.Name)]
+        fn_names = [n.func.id for n in calls if isinstance(n.func, ast.Name)]
+
+        if 'no-clear' not in opts:
+            fns = (local_ns[name] for name in fn_names if name in local_ns)
+            clear_fns = filter(
+                None, (getattr(fn, 'cache_clear', None) for fn in fns))
+            list(c() for c in clear_fns if hasattr(c, '__call__'))
 
         # For now, only global variables (`a(x)`) are instrumented, not
         # attributes (`a.b(x)`). The following would collect attributes
         # ("qualified" function calls).
         # qfns = list(n.func for n in calls if isinstance(n.func, ast.Attribute))
 
-        with CallGraphInstrumentor(fns, recorder=recorder, local_ns=local_ns) as recorder:
+        with CallGraphInstrumentor(fn_names, recorder=recorder, local_ns=local_ns) as recorder:
             exec(compile(tree, filename=filename, mode=mode), local_ns)
             display(SVG(data=recorder.graph._repr_svg_()))
 
