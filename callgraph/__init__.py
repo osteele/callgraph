ea58b21fa99ce9a48315af12f346c197abf5d505
Oliver Steele <steele@osteele.com>
2018-01-02 22:09:51 -0500

    Add description-file; bump version

--- callgraph/__init__.py
+++ callgraph/__init__.py
@@ -1,6 +1,6 @@
 """Decorators and Jupyter IPython magic to display a dynamic call graph."""
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from .extension import load_ipython_extension
 from .decorator import decorator
