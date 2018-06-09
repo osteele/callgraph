5b0befa838b875daa92f3431a434502e0d929f1e
Oliver Steele <steele@osteele.com>
2018-06-09 12:07:56 -0400

    Bump version: 0.1.4 → 1.0.0

--- callgraph/__init__.py
+++ callgraph/__init__.py
@@ -1,6 +1,6 @@
 """This package defines decorators and IPython magic to display a dynamic call graph."""
 
-__version__ = '0.1.4'
+__version__ = '1.0.0'
 __all__ = ['load_ipython_extension', 'decorator', 'CallGraphRecorder']
 
 from .extension import load_ipython_extension
--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ author = 'Oliver Steele'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.4'
+version = '1.0.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
