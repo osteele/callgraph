b292279fe193e969488febc234c04f22b00dc976
Oliver Steele <steele@osteele.com>
2018-06-09 12:07:22 -0400

    Add bumpversion config

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ author = 'Oliver Steele'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.3'
+version = '0.1.4'
 # The full version, including alpha/beta/rc tags.
 release = version
 
